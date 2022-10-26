c664c9e8d65c0445b4693ea19bc8c62c30e2cb7c
Winston Langat <winston.langat@student.moringaschool.com>
2022-10-26 17:08:33 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
-
-end
+class Student < User
+    ## attr_accessor :knowledge
+         def initialize(knowledge=[])
+           @knowledge = knowledge
+         end
+     def learn(new_knowledge)
+       @knowledge << new_knowledge
+     end
+     def knowledge
+       return @knowledge
+     end
+   
+   end
